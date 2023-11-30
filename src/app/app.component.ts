e30deff43b261a947c0f9599265a644a74f82b6c
Dev Chauhan <dev.chauhan@hexaviewtech.com>
2023-11-30 17:08:03 +0530

    ng form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgForm-Project';
+  userData:any =[];
+  dataHeaders:any=[]
+
+  getData(data:NgForm) {
+    this.userData = [...this.userData, data]; 
+    this.dataHeaders = Object.keys(data);
+  }
 }
