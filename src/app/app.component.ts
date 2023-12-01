02247c7a4e102cc87c2c6d5a46439ff0d6bbac52
Parvez <parvez@hexaviewtech.com>
2023-12-01 15:47:09 +0530

    added loginc for edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,9 @@ export class AppComponent {
   title = 'NgForm-Project';
   userData:any =[];
   dataHeaders:any=[];
+  modalData:any={};
+  showModal:boolean = false;
+  editDataIndex:number = -99;
 
   getData(data:NgForm) {
     this.userData = [...this.userData, data];
@@ -21,7 +24,22 @@ export class AppComponent {
   }
 
   editRow(index:number){
+    const rowData = this.userData[index]
+    this.modalData = rowData;
+    this.editDataIndex = index;
+    this.showModal = true
+  }
 
+  setEditModalRecord(data:NgForm) {
+    if(this.editDataIndex != -99){
+      this.userData[this.editDataIndex] = data;
+    }
+    this.closeModal()
+  }
 
+  closeModal() {
+    this.editDataIndex = -99;
+    this.showModal = false;
   }
 }
+
