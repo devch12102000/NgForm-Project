749bc6f184e22cd782cf85397ddace93e1b877d6
Aarushi Tyagi <aarushi.tyagi002@gmail.com>
2023-12-02 17:06:04 +0530

    split pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,55 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  //@ViewChild('basicForm') basicForm: NgForm;
   title = 'NgForm-Project';
   userData:any =[];
   dataHeaders:any=[];
+  //modal:any={};
   modalData:any={};
   showModal:boolean = false;
   editDataIndex:number = -99;
-  
+  personaldetail:boolean=true;
+  professionaldetail=false;
+  identitydetail=false;
+
+
+  constructor(
+    // private readonly fb: FormBuilder
+  ){ }
+ngOnInit(): void {
+
+}
+// reset(basicForm:NgForm){
+//   basicForm.reset()}
 
   getData(data:NgForm) {
+    debugger
     this.userData = [...this.userData, data];
     this.dataHeaders = [...Object.keys(data),"actions"];
+    //this.basicForm.form.value.reset();
     console.log("da", data)
+
   }
+
+  // getData(data:NgForm) {
+  // const formData = new FormData();
+
+  //   formData.append('name', this.userData.Name);
+  //   formData.append('postal_address', this.userData.Address);
+  //   formData.append('designation', this.userData.Gender);
+  //   formData.append('contact_person', this.userData.Role);
+  //   formData.append('contact_person', this.userData.Citizenship);
+  //   formData.append('contact_person', this.userData.uploadfile);
+  //   console.log("formData", formData);
+  // }
 
   deleteRow(index:number){
     this.userData.splice(index,1) //splice(from what index, how many elements to delete)
@@ -30,6 +60,7 @@ export class AppComponent {
     this.modalData = rowData;
     this.editDataIndex = index;
     this.showModal = true;
+    console.log(this.modalData.Name)
   }
 
   setEditModalRecord(data:NgForm) {
@@ -43,5 +74,34 @@ export class AppComponent {
     this.editDataIndex = -99;
     this.showModal = false;
   }
+
+
+
+  NextPersonalDetail(){
+    this.personaldetail=false;
+    this.professionaldetail=true; //takes to professional page 2
+    this.identitydetail=false;
+  }
+
+  NextProfessionalDetail(){
+    this.personaldetail=false;
+    this.professionaldetail=false;
+    this.identitydetail=true; //takes to identity page 3
+  }
+
+  BackProfessionalDetail(){
+    this.personaldetail=true; //takes to personal page 1
+    this.professionaldetail=false;
+    this.identitydetail=false;
+
+  }
+
+  BackIdentityDetail(){
+    this.personaldetail=false;
+    this.professionaldetail=true; //takes to professional page 1
+    this.identitydetail=false;
+
+  }
+
 }
 
